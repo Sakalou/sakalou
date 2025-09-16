7b2002dcaf3c323ae2cf5af61f1af3a1a45637b1
Yauheni Sakalou <jekasokoloff@gmail.com>
2025-09-16 23:50:25 +0300

    Add meta tags in head

--- src/routes/rides/[slug]/+page.svelte
+++ src/routes/rides/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Article from '$lib/components/content/Article.svelte';
+  import { page } from '$app/stores';
 
   export let data;
 
@@ -8,6 +9,18 @@
 
 <svelte:head>
   <title>{data.metadata.title}</title>
+
+  <meta property="og:title" content={data.metadata.description} />
+  {#if data.metadata.image}
+    <meta
+      property="og:image"
+      content="https://ik.imagekit.io/sakalou{$page.url.pathname}/{data.metadata
+        .image}?tr=w-1200,f-webp,q-70"
+    />
+  {/if}
+  <meta property="og:url" content="https://sakalou.by{$page.url.pathname}" />
+  <meta property="og:type" content="article" />
+  <meta property="twitter:card" content="summary_large_image" />
 </svelte:head>
 
 <Article article={component} title={data.metadata.title} date={metadata.date} />
