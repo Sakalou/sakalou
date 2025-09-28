fd0d8e62e93feb41b4ed2d65c852db6f1754f2ef
Yauheni Sakalou <jekasokoloff@gmail.com>
2025-09-29 00:57:01 +0300

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,3 +16,22 @@
 <main class="py-8">
   {@render children()}
 </main>
+
+<footer class="grid grid-cols-(--grid-cols-layout) justify-center py-4 text-sm">
+  <ul class="col-2">
+    <li>
+      Телеграм-канал: <a
+        href="https://t.me/sakalounotes"
+        target="_blank"
+        class="text-sky-600 underline underline-offset-4 hover:no-underline">@sakalounotes</a
+      >
+    </li>
+    <li>
+      Инстаграм: <a
+        href="https://www.instagram.com/sakalou/"
+        target="_blank"
+        class="text-sky-600 underline underline-offset-4 hover:no-underline">@sakalou</a
+      >
+    </li>
+  </ul>
+</footer>
