7bc7533561e7ad3da0476358be7055585c8f62c5
Yauheni Sakalou <jekasokoloff@gmail.com>
2025-10-06 00:43:58 +0300

    Add rides page

--- src/routes/rides/+page.svelte
+++ src/routes/rides/+page.svelte
@@ -6,7 +6,7 @@
   <title>Велоотчеты | Сакалоў</title>
 </svelte:head>
 
-<section class="mx-auto w-full max-w-3xl">
+<section class="mx-auto w-full">
   <h1
     class="
       mx-auto
@@ -20,14 +20,20 @@
     Велоотчеты
   </h1>
 
-  <ul class="mx-auto grid max-w-3xl list-disc gap-2 pl-6">
-    {#each content.rides as { slug, title } (slug)}
+  <ul class="mx-auto grid max-w-3xl gap-8 sm:grid-cols-2">
+    {#each content.rides as { slug, title, image, category } (slug)}
+      {@const src = `https://ik.imagekit.io/sakalou/${category}/${slug}/${image}`}
       <li>
-        <a
-          href="rides/{slug}"
-          class="text-sky-600 underline underline-offset-4 visited:text-violet-800 hover:no-underline"
-          >{title}</a
-        >
+        <a href="rides/{slug}" class="text-black underline-offset-4 hover:underline">
+          <img
+            class="aspect-4/3 w-full border-b-8 border-b-transparent"
+            {src}
+            sizes="(min-width: 600px) 368px, 100vw"
+            srcset="{src}?tr=w-1440,h-1080,f-webp,q-70 1440w, {src}?tr=w-1080,h-768,f-webp,q-70 1080w, {src}?tr=w-736,h-552,f-webp,q-70 736w, {src}?tr=w-368,h-276,f-webp,q-70 368w"
+            alt={title}
+          />
+          {title}
+        </a>
       </li>
     {/each}
   </ul>
